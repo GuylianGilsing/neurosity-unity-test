108d0c24113dce841418a8821fa130072905c337
JulianJ99 <julian99janssen@gmail.com>
2024-05-29 12:43:59 +0200

    Commented debug logs

--- neurosity-unity-test/Assets/Scripts/UI/SliderWaves.cs
+++ neurosity-unity-test/Assets/Scripts/UI/SliderWaves.cs
@@ -16,35 +16,33 @@ public class SliderWaves : MonoBehaviour
     [SerializeField] private Image deltaChannel;
     [SerializeField] private Image gammaChannel;
     [SerializeField] private Image thetaChannel;
-    [SerializeField] private int hertRange = 250;
 
-    DisplayBrainWaveBands waveBands;
 
     // Start is called before the first frame update
     void Start()
     {
         _alphaSlider.onValueChanged.AddListener((a) => {
-            Debug.Log(_alphaSlider.value);
+            //Debug.Log(_alphaSlider.value);
             this.alphaChannel.color = new Color(_alphaSlider.value / 200, 0, 0);
         });
 
         _betaSlider.onValueChanged.AddListener((b) => {
-            Debug.Log(_betaSlider.value);
+            //Debug.Log(_betaSlider.value);
             this.betaChannel.color = new Color(0, _betaSlider.value / 200, 0);
         });
 
         _deltaSlider.onValueChanged.AddListener((d) => {
-            Debug.Log(_deltaSlider.value);
+            //Debug.Log(_deltaSlider.value);
             this.deltaChannel.color = new Color(0, 0, _deltaSlider.value / 200);
         });
 
         _gammaSlider.onValueChanged.AddListener((g) => {
-            Debug.Log(_gammaSlider.value);
+            //Debug.Log(_gammaSlider.value);
             this.gammaChannel.color = new Color(_gammaSlider.value / 200, 0, 0);
         });
 
         _thetaSlider.onValueChanged.AddListener((t) => {
-            Debug.Log(_thetaSlider.value);
+            //Debug.Log(_thetaSlider.value);
             this.thetaChannel.color = new Color(0, 0, _thetaSlider.value / 200);
         });
     }
